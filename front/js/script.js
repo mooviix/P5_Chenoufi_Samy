2a1e24dd6364b1971557d5bfd40b95db72969125
samy <samy.chenoufi3@gmail.com>
2022-05-24 11:45:01 +0200

    init

---
+++ front/js/script.js
@@ -0,0 +1,41 @@
+fetch("http://localhost:3000/api/products")
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(data) {
+    console.log(data);
+    for (let product in data){
+
+      // déclaration des variables pour index.html
+
+      let section = document.getElementById("items");
+      let lien = document.createElement("a");
+      let article = document.createElement("article");
+      let img = document.createElement("img");
+      let titre = document.createElement("h3");
+      let description = document.createElement("p");
+
+      // création des class pour index.html
+
+      titre.classList.add("productName");
+      description.classList.add("productDescription");
+
+      // Ajout des enfants dans index.html
+
+      section.appendChild(lien);
+      lien.appendChild(article);
+      article.appendChild(img);
+      article.appendChild(titre);
+      article.appendChild(description);
+      
+      // ajout du contenu dans l'élement
+
+      img.src = data[product].imageUrl;
+      img.alt = data[product].altTxt;
+      titre.innerHTML = data[product].name;
+      description.innerHTML = data[product].description;
+      lien.href = `product.html?id=${data[product]._id}`;
+    }
+  });
